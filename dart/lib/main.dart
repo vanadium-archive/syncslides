6b5d58c55fc3d9fd9b97695a85875a03faf91664
Ali Ghassemi <aghassemi@google.com>
2015-11-18 13:18:56 -0800

    syncslides: Integrating the new back button event with the app.
    Closes: https://github.com/vanadium/syncslides/issues/2
    Change-Id: I5cd3fc6f5a1d9fc836160b8de7157da5241886a5

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -7,19 +7,42 @@ import 'package:logging/logging.dart';
 
 import 'styles/common.dart' as style;
 import 'components/deckgrid.dart';
+import 'utils/back_button.dart' as backButtonUtil;
+
+NavigatorState _navigator;
 
 void main() {
   _initLogging();
+  _initBackButtonHandler();
 
   runApp(new MaterialApp(
       theme: style.theme,
       title: 'SyncSlides',
-      routes: {'/': (RouteArguments args) => new DeckGridPage()}));
+      routes: {'/': (RouteArguments args) => new LandingPage()}));
+}
+
+class LandingPage extends StatelessComponent {
+  Widget build(BuildContext context) {
+    _navigator = Navigator.of(context);
+    return new DeckGridPage();
+  }
 }
 
 void _initLogging() {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((LogRecord rec) {
     print('\nSyncSlides: ${rec.time}: ${rec.message}');
+  });
+}
+
+void _initBackButtonHandler() {
+  backButtonUtil.onBackButton(() {
+    if (_navigator != null && _navigator.hasPreviousRoute) {
+      _navigator.pop();
+      return true;
+    }
+
+    // Tell the app to exit.
+    return false;
   });
 }
---
+++ dart/lib/utils/back_button.dart
@@ -0,0 +1,40 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/services.dart' show shell;
+import 'package:mojo_services/input/input.mojom.dart';
+
+String _inputMojoUrl = 'mojo:input';
+
+typedef bool BackButtonHandler();
+
+BackButtonHandler _handler;
+// Registers a handler for the back button.
+// Handler should return false when it no longer wants to control
+// the behaviour of the back button, in which case the app will exit.
+void onBackButton(BackButtonHandler handler) {
+  if (_handler != null) {
+    throw new ArgumentError("Only one back button handler can exist per app.");
+  }
+
+  InputServiceProxy inputService = new InputServiceProxy.unbound();
+  shell.connectToService(_inputMojoUrl, inputService);
+
+  InputClientStub intputClientStub = new InputClientStub.unbound();
+  intputClientStub.impl = new _InputHandler();
+
+  inputService.ptr.setClient(intputClientStub);
+  _handler = handler;
+}
+
+class _InputHandler extends InputClient {
+  dynamic onBackButton([Function responseFactory]) {
+    bool exit = !_handler();
+    if (exit) {
+      return null;
+    } else {
+      return responseFactory();
+    }
+  }
+}
